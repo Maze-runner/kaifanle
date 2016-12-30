f5e8496dda3ebf843e33bde6fdbb9ccb06c4d892
Maze-runner <394300118@qq.com>
2016-12-30 21:02:10 +0800

    基本模板搭建完成

--- js/kaifanla.js
+++ js/kaifanla.js
@@ -2,26 +2,54 @@
  * Created by Administrator on 2016/12/30.
  */
 
-// 定义模块
+// 定义模块 kaifanla
 var app = angular.module('kaifanla',['ng','ngRoute']);
 
 // 定义路由词典
 app.config(function($routeProvider){
     $routeProvider.when('/start',{
-        templateUrl:'tpl/start.html'
+        templateUrl:'tpl/start.html',          // start路由
+        controller:'startCtr'
     }).when('/main',{
-        templateUrl:'tpl/main.html'
+        templateUrl:'tpl/main.html',           // main路由
+        controller:'mainCtr'
     }).when('/detail',{
-        templateUrl:'tpl/detail.html'
+        templateUrl:'tpl/detail.html',         // detail路由
+        controller:'detailCtr'
     }).when('/order',{
-        templateUrl:'tpl/order.html'
-    }).when('/myorder',{
-        templateUrl:'tpl/myorder.html'
-    }).otherwise({ redirectTo:'/start' })
+        templateUrl:'tpl/order.html',            // order路由
+        controller:'orderCtr'
+    }).when('/myOrder',{
+        templateUrl:'tpl/myOrder.html',        // myOrder路由
+        controller:'myOrderCtr'
+    }).otherwise({ redirectTo:'/start' })      // 默认路由start
 })
 
 
 // 定义body父元素控制器
-app.controller('bodyCtr',['$scope',function($scope){
+app.controller('bodyCtr',['$scope','$location','$http',function($scope,$location){
+
+    $scope.jump = function(arg){
+        $location.path(arg);
+    }
+}])
+// start控制器
+app.controller('startCtr',['$scope',function($scope){
+
+}])
+// main控制器
+app.controller('mainCtr',['$scope',function($scope){
+
+}])
+// detail控制器
+app.controller('detailCtr',['$scope',function($scope){
+
+}])
+// order控制器
+app.controller('orderCtr',['$scope',function($scope){
+
+}])
+// myOrder控制器
+app.controller('myOrderCtr',['$scope',function($scope){
 
 }])
